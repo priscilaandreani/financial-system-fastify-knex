3182ab6c135964c034a66f580a271272debfeb04
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-08-07 17:26:34 -0300

    feat: update database configuration and add migrations

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import { knex as knexSetup } from 'knex';
+import { knex as knexSetup, Knex } from 'knex';
 
-export const knex = knexSetup({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/sqlite.db'
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './tmp/migrations'
   }
-});
+};
+
+export const knex = knexSetup(config);
