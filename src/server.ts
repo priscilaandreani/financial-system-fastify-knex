068696840041a10340a68b76fe506fd76c768169
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-08-07 15:08:58 -0300

    chore: config prettier

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const server = fastify();
 
-server.get("/hello", () => {
-  return "Hello World, watch";
+server.get('/hello', () => {
+  return 'Hello World, watch';
 });
 
 server
   .listen({
-    port: 3333,
+    port: 3333
   })
   .then(() => {
-    console.log("HTTP Server is running on port 3333");
+    console.log('HTTP Server is running on port 3333');
   });
