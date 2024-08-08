e6ccdc7d58b973fa61c6183d558853f1a61bc652
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-08-08 10:15:53 -0300

    feat: add transaction creation endpoint to server

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,16 +1,24 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 import { knex } from '../database';
+import crypto from 'node:crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 1000
-      })
-      .returning('*');
+  app.post('/', async (req, res) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    });
 
-    return transaction;
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body);
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'debit' ? amount * -1 : amount
+    });
+
+    return res.status(201).send();
   });
 }
