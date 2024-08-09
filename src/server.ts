f75e48f00c60f48a494501631be88bba42692c45
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-08-09 14:25:51 -0300

    refactor: separate app initialization logic into app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,4 @@
-import fastify from 'fastify';
-import { transactionsRoutes } from './routes/transactions';
-import cookie from '@fastify/cookie';
-
-const app = fastify();
-
-app.register(cookie);
-
-app.register(transactionsRoutes, {
-  prefix: '/transactions'
-});
+import { app } from './app';
 
 app.listen(
   {
