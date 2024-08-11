c2c24f502d64d55f804a0b2256e18d5bba9346bd
priscilaandreani <priscilaandreani@gmail.com>
2024-08-11 16:46:16 -0300

    build: add build script for TypeScript compilation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
+  {
+    ignores: ['dist/**/*', 'node_modules/**/*']
+  }
 ];
