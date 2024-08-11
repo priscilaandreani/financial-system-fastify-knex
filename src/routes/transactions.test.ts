719da25c389091790eab601015b1a350860329e1
priscilaandreani <priscilaandreani@gmail.com>
2024-08-11 16:03:23 -0300

    test: add transaction summary route

--- src/routes/transactions.test.ts
+++ src/routes/transactions.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import { app } from '../../src/app';
-import { it, beforeAll, afterAll, describe, beforeEach } from 'vitest';
+import { it, beforeAll, afterAll, describe, beforeEach, expect } from 'vitest';
 import { execSync } from 'child_process';
 
 beforeAll(async () => {
@@ -45,6 +45,29 @@ describe('Transactions routes', () => {
       .expect(200);
   });
 
+  it('should be able to get a transaction by id', async () => {
+    const createTransactionRes = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'burger',
+        amount: 1300,
+        type: 'credit'
+      });
+
+    const cookies = createTransactionRes.headers['set-cookie'];
+
+    const transactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies);
+
+    const transactionId = transactions.body.transactions[0].id;
+
+    await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200);
+  });
+
   it('should be able to delete a transaction', async () => {
     const createTransactionRes = await request(app.server)
       .post('/transactions')
@@ -66,5 +89,36 @@ describe('Transactions routes', () => {
       .delete(`/transactions/${transactionId}`)
       .set('Cookie', cookies)
       .expect(204);
+  });
+
+  it('should be able to get summary', async () => {
+    const createCreditTransactionRes = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'salary',
+        amount: 1300,
+        type: 'credit'
+      });
+
+    const cookies = createCreditTransactionRes.headers['set-cookie'];
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'burger',
+        amount: 50,
+        type: 'debit'
+      });
+
+    const summaryRes = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies);
+
+    expect(summaryRes.body).toEqual({
+      summary: {
+        total: 1250
+      }
+    });
   });
 });
